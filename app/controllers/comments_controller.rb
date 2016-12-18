c9eae9a32418352c35dd90b5cb99598922ad762c
Sheldon Bazzell <sheldon.bazzell@gmail.com>
2016-12-17 19:26:35 -0800

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
 
+	# create Comment object
 	def create
+		# call comment_params method to ensure data is restricted to
+		# what is expected in Comment model
 		@comment = Comment.new(comment_params)
+		# if the user's inputs didn't pass the Comment model's validations, assign flash
 		unless @comment.valid?
 			flash[:comment_error] = @comment.errors.full_messages.to_sentence
+		# else, save the Comment object, and associate to logged in user
 		else
 			@comment.save!
 			@comment.update(user:current_user)

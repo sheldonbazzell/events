5e1187e95d35bbef7db50aa0818e41f24f977c61
Sheldon Bazzell <sheldon.bazzell@gmail.com>
2016-12-17 19:34:28 -0800

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,43 @@
 class UsersController < ApplicationController
-  def index
-  end
 
+  # make sure client inputs are what is expected 
+  # of User class attributes
   def create
   	@user = User.new( user_params )
-  	unless @user.valid?
-      if @user.errors.include?(:first_name) and @user.errors.include?(:last_name) and @user.errors.include?(:email) and @user.errors.include?(:city) and @user.errors.include?(:password)
-        flash[:errors] = "All fields required"
-      else
+  	# if inputs don't pass validations, assign flash
+    unless @user.valid?
         flash[:errors] = @user.errors.full_messages.to_sentence
-      end
     		redirect_to "/users"
+    # if inputs pass validations, save new user
   	else
   		@user.save!
-<<<<<<< HEAD
+      # store email in all lowercase to ensure case
+      # insensitivty on login
       @user.update(email:params[:user][:email].downcase)
-=======
-      @user.update(email:@user.email.downcase)
->>>>>>> e20388576a44580dbec3a31787a5fb29b67c44a8
-  		session[:id] = @user.id
+  		# set the session for the newly registered user
+      session[:id] = @user.id
   		redirect_to "/events"
   	end
   end
 
-  def edit
-  end
-
+  
   def update
     @user = current_user
+    # check to see if client's update params match
+    # expectations in User class
     @user.update(user_params)
+    # if the inputs don't pass validations, assign flash
     if !@user.valid?
       flash[:errors] = @user.errors.full_messages.to_sentence
       redirect_to "/users/#{current_user.id}/edit"
+    # if inputs pass validations, save the updates
     else
       @user.save!
       redirect_to "/events"
     end
   end
 
+  
   private
   def user_params
   	params.require(:user).permit(:first_name, :last_name, :email, :city, :state, :password, :password_confirmation)
